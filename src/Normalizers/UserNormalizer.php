464a037fed918f0787c9152f0ff715dd80c2f3f7
Grzegorz Chrymecki <greg@soft4u.uk>
2023-04-29 13:08:37 +0100

    user normalizer

---
+++ src/Normalizers/UserNormalizer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Street\App\Normalizers;
+
+use Street\App\Model\User;
+
+class UserNormalizer extends BaseNormalizer
+{
+
+    function normalize()
+    {
+        // TODO: Implement normalize() method.
+    }
+
+
+}
--- src/index.php
+++ src/index.php
@@ -2,14 +2,29 @@
 
 use Street\App\File\CsvFileIterator;
 use Street\App\Normalizers\CsVNormalizer;
+use Street\App\Normalizers\UserNormalizer;
 
 require __DIR__ .  '/../vendor/autoload.php';
 
 
 $csv_file_iterator = new CsvFileIterator(__DIR__ .'/take-home-task-data.csv');
+
 $csv_normalizer = new CsVNormalizer();
 $csv_file_iterator->addDataNormalizer($csv_normalizer);
-
+// get properly structured data for further processing (get uniform data)
 $csv_normalizer->normalize();
 
-print_r($csv_normalizer->getData());
+$user_normalizer = new UserNormalizer();
+
+foreach ($csv_normalizer->getData() as $row){
+    $user_normalizer->addData($row);
+}
+
+// uniform data will be normalized to User Model
+// and will be ready to store in database
+$user_normalizer->normalize();
+
+$result = $user_normalizer->getData();
+
+print_r($result);
+
